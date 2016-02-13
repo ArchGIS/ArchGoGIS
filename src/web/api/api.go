e089f8c01eb09f253d2e39d8488bfa7c3a3bacb6
quasilyte <quasilyte@gmail.com>
2016-02-13 18:48:59 +0300

    пакет для web api response'ов

---
+++ src/web/api/api.go
@@ -0,0 +1,11 @@
+package api
+
+const NoError = `{"error":false}`
+
+func InternalServerError() string {
+	return `{"error":"internal server error"}`
+}
+
+func Error(err error) string {
+	return `{"error":"` + err.Error() + `"}`
+}
