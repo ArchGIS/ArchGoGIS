ec760a835987827cf7020ad3b42728b11d0aa01b
quasilyte <quasilyte@gmail.com>
2016-02-23 23:03:27 +0300

    извлекать id нам пока выгоднее пачками

--- src/db/pg/seq/seq.go
+++ src/db/pg/seq/seq.go
@@ -3,6 +3,7 @@ package seq
 import (
 	"db/pg"
 	"errors"
+	"fmt"
 )
 
 // Префикс n_ для нодов, e_ - для рёбер
@@ -18,23 +19,39 @@ var idSequences = map[string]string{
 // с бд будет утрачена и все подготовленные запросы перестанут быть
 // валидными. Поэтому как минимум пока - keep it simple.
 
-func NextId(tableName string) (string, error) {
+func NextIds(tableName string, n int) ([]string, error) {
+	if n == 0 {
+		return nil, errors.New("cant fetch 0 ids")
+	}
+
 	if seqName, ok := idSequences[tableName]; ok {
-		return NextString(seqName)
+		ids := make([]string, n)
+
+		rows, err := pg.Agent.Query(
+			fmt.Sprintf("SELECT NEXTVAL('%s') FROM GENERATE_SERIES(1, %d)", seqName, n),
+		)
+		if err != nil {
+			return nil, err
+		}
+		defer rows.Close()
+
+		for i := 0; rows.Next(); i += 1 {
+			if err := rows.Scan(&ids[i]); err != nil {
+				return nil, err
+			}
+		}
+		if err := rows.Err(); err != nil {
+			return nil, err
+		}
+
+		return ids, nil
 	} else {
-		return 0, errors.New("id sequence for " + tableName + " not found")
+		return nil, errors.New("id sequence for " + tableName + " not found")
 	}
 }
 
 func NextInt(seqName string) (int, error) {
 	var val int
-	err := pg.Agent.QueryRow("SELECT NEXTVAL('" + seqName + "')").Scan(&val)
-
-	return val, err
-}
-
-func NextString(seqName string) (string, error) {
-	var val string
 	err := pg.Agent.QueryRow("SELECT NEXTVAL('" + seqName + "')").Scan(&val)
 
 	return val, err
