055b327c0582128d2cc55f62740fa7ffcb73818a
quasilyte <quasilyte@gmail.com>
2016-02-23 13:59:20 +0300

    новый логгер (vendor errors)

--- src/echo/echo.go
+++ src/echo/echo.go
@@ -10,6 +10,7 @@ var (
 	Warning     *log.Logger // Что-то подозрительное на стороне сервера
 	ServerError *log.Logger // Ошибка на стороне сервера. Что-то ужасное
 	ClientError *log.Logger // Ошибка со стороны клиента, ничего страшного
+	VendorError *log.Logger // Неожиданное поведение со стороны библиотеки
 )
 
 func init() {
@@ -19,4 +20,5 @@ func init() {
 	Warning = log.New(os.Stdout, "{Warning} ", flags)
 	ServerError = log.New(os.Stderr, "{ServerError} ", flags)
 	ClientError = log.New(os.Stdout, "{ClientError} ", flags)
+	VendorError = log.New(os.Stderr, "{VendorError} ", flags)
 }
