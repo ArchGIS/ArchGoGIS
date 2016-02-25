49d08951463435235e6240091d2c0dc378b779d1
quasilyte <quasilyte@gmail.com>
2016-02-25 13:53:13 +0300

    восстановил табуляцию

--- fs/web_client/js/init/router.js
+++ fs/web_client/js/init/router.js
@@ -2,33 +2,33 @@
 
 App.router = new (Backbone.Router.extend({
   'currentController': {},
-	
+  
   'routes': {
-	'*actions': function(dispathInfo) {
-		
+    '*actions': function(dispathInfo) {
+      
       if (!dispathInfo) {
-		return; // Скорее всего, это root action, но мы его пока не обрабатываем.
+	return; // Скорее всего, это root action, но мы его пока не обрабатываем.
       }
-		
+      
       // Предварительная очистка.
       if (this.currentController.destruct) {
         this.currentController.destruct();
-	  }
-	  App.$body.empty();
-		
+      }
+      App.$body.empty();
+      
       // Далее пытаемся запустить action контроллера:
-	
+      
       var controllerNameAndAction = dispathInfo.split('/');
       if (controllerNameAndAction.length != 2) {
-		throw 'invalid controller/action: ' + controllerNameAndAction;
+	throw 'invalid controller/action: ' + controllerNameAndAction;
       } 
-	
+      
       var controllerName = controllerNameAndAction[0];
-	  var controllerAction = controllerNameAndAction[1];
+      var controllerAction = controllerNameAndAction[1];
       if (!_.has(App.controllers, controllerName)) {
         throw 'controller ' + controllerName + ' is undefined';
       }
-	
+      
       if (controllerAction.length == 0) {
         controllerAction = 'index';
       }
@@ -36,10 +36,10 @@ App.router = new (Backbone.Router.extend({
       if (!App.controllers[controllerName][controllerAction]) {
         throw 'controller ' + controllerName + ' has no action ' + controllerAction;
       }
-	
-	  this.currentController = App.controllers[controllerName];
+      
+      this.currentController = App.controllers[controllerName];
       App.controllers[controllerName][controllerAction]();
-	}
+    }
   }
 }));
 
