acfd4b743b2aacf6a0327fd83f414cf856b560fc
bontequero <bontequero@localhost.pc>
2016-03-16 17:33:46 +0300

    добавление просмотра памятника

--- fs/web_client/js/controllers/monument.js
+++ fs/web_client/js/controllers/monument.js
@@ -1,8 +1,29 @@
 'use strict';
 
+App.monuments = {
+  '1': {
+    'monumentName': 'Болгар',
+    'monumentX': '12.3232',
+    'monumentY': '53.5353',
+    'monumentType': 'Городище',
+    'monumentEpoch': 'Кайнозой'
+  },
+  '2': {
+    'monumentName': 'Кармыш',
+    'monumentX': '7.3232',
+    'monumentY': '29.5353',
+    'monumentType': 'Холм',
+    'monumentEpoch': 'Мезозой'
+  }
+};
+
 App.controllers.monument = new (App.View.extend({
+  'index': function() {
+    App.page.render('monument', App.monuments[id]);
+  },
+
   'new': function() {
-    App.page.render('monument', {'param': 'test data'});
+    console.log('new monument creating');
   },
 
   'start': function() {
