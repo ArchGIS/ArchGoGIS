b459a3089b5ce068b18747547b9361a6de493bd0
quasilyte <quasilyte@gmail.com>
2016-03-16 11:07:15 +0300

    наброски для hquery/delete

---
+++ src/service/hquery/delete/delete.go
@@ -0,0 +1,66 @@
+package delete
+
+import (
+	// "encoding/json"
+	// "fmt"
+	"io"
+	"net/http"
+	"service/hquery/errs"
+	"service/hquery/parsing"
+	"service/hquery/valid"
+	"throw"
+	"web"
+)
+
+/*
+{
+  "Author": {
+    "119": "*",
+    "112": "*",
+    "120": "name"
+  }
+}
+
+заметки:
+x.id IN [id...] лучше, чем x.id = 1 or x.id = 2 ...
+без detach удаление будет быстрее
+для props оператор удаления - remove
+каждый "lables" должен идти в отдельном окне
+
+*/
+
+type Parser struct {
+	input map[string]map[string]string
+}
+
+func mustNewParser(input io.ReadCloser) *Parser {
+	this := &Parser{input: parsing.MustFetchJson(input)}
+
+	return this
+}
+
+func (my *Parser) mustParse() {
+	/*
+		for labels, pairs := range my.input {
+			for id, selector := range pairs {
+				throw.If(!valid.Number(id), errs.PropInvalidNumber)
+				throw.If(selector != "*" && !valid.Identifier(selector), errs.QueryBadSelector)
+			}
+		}
+	*/
+}
+
+func Handler(w web.ResponseWriter, r *http.Request) {
+	// parser := mustNewParser(r.Body)
+	// mustValidate(data)
+}
+
+func mustValidate(data map[string]map[string]string) {
+	for labels, pairs := range data {
+		throw.If(!valid.Identifier(labels), errs.InvalidIdentifier)
+		for id, selector := range pairs {
+			throw.If(!valid.Number(id), errs.PropInvalidNumber)
+			throw.If(selector != "*" && !valid.Identifier(selector), errs.QueryBadSelector)
+		}
+	}
+}
