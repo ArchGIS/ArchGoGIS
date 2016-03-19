6aca1fcb40f553206b0e05bf1e424fb460f2e3a8
quasilyte <quasilyte@gmail.com>
2016-03-19 11:28:52 +0300

    не делаем лишних запросов к серверу

--- fs/web_client/js/views/monument.js
+++ fs/web_client/js/views/monument.js
@@ -2,15 +2,15 @@
 
 App.views.monument = new (App.View.extend({
   'new': function() {
-    App.page.get('authorInput').on('autocompleteselect', function(event, ui) {
+    var authorSelectHandler = function(event, ui) {
       console.log(ui.item.id);
       $('#author-input-id').val(ui.item.id);
-
+      
       var args = {
         'r:Research': {'id': '*', 'select': '*'},
         'a:Author': {'id': $('#author-input-id').val()},
         'a_Created_r': {}
-      }
+      };
       var researches = [];
 
       $.post('/hquery/read', JSON.stringify(args), function(data) {
@@ -24,50 +24,58 @@ App.views.monument = new (App.View.extend({
           source: researches
         })
       });
-    });
-    
-    $("#research-input").autocomplete({
-      source: [],
-      minLength: 0,
-      select: function(event, ui) { 
-        $("#research-input-id").val(ui.item.id);
-        
-        var args = {
-          "monument:Monument": {"id": "*", "select": "*"},
-          "researches:Research": {"id": $("#research-input-id").val()},
-          "knowledge:Knowledge": {"id": "*", "select": "*"},
-          "researches_Contains_knowledge": {},
-          "knowledge_Describes_monument": {}
-        }
-        var monuments = [];
+      
+      $("#research-input").autocomplete({
+        source: [],
+        minLength: 0,
+        select: function(event, ui) { 
+          $("#research-input-id").val(ui.item.id);
+          
+          var args = {
+            "monument:Monument": {"id": "*", "select": "*"},
+            "researches:Research": {"id": $("#research-input-id").val()},
+            "knowledge:Knowledge": {"id": "*", "select": "*"},
+            "researches_Contains_knowledge": {},
+            "knowledge_Describes_monument": {}
+          }
+          var monuments = [];
 
-        $.post(dburl+"hquery/read", JSON.stringify(args), function(data) {
-          console.log(data)
-          var data = $.parseJSON(data).knowledge;
-          var title;
-          $.each(data, function(key, res) {
-            title = res.name+" ("+res.culture+")";
-            monuments.push({label: title, id: res.id})
-          });
-          $("#monument-input").autocomplete({
-            source: monuments
+          $.post(dburl+"hquery/read", JSON.stringify(args), function(data) {
+            console.log(data)
+            var data = $.parseJSON(data).knowledge;
+            var title;
+            $.each(data, function(key, res) {
+              title = res.name+" ("+res.culture+")";
+              monuments.push({label: title, id: res.id})
+            });
+            $("#monument-input").autocomplete({
+              source: monuments
+            })
           })
-        })
-      }
-    }).focus(function(){            
-      $(this).autocomplete("search");
-    });
+        }
+      }).focus(function(){            
+        $(this).autocomplete("search");
+      });
 
-    $("#monument-input").autocomplete({
-      source: function(request, response) {
-        response(window[""+$("#research-input-id").val()]);
-      },
-      minLength: 0,
-      select: function(event, ui) { 
-        $("#monument-input-id").val(ui.item.id);
-      }
-    }).focus(function(){            
-      $(this).autocomplete("search");
+      $("#monument-input").autocomplete({
+        source: function(request, response) {
+          response(window[""+$("#research-input-id").val()]);
+        },
+        minLength: 0,
+        select: function(event, ui) { 
+          $("#monument-input-id").val(ui.item.id);
+        }
+      }).focus(function(){            
+        $(this).autocomplete("search");
+      });
+    };
+  
+    var lastSelectedAuthorId = 0;
+    App.page.get('authorInput').on('autocompleteselect', function(event, ui) {
+      if (lastSelectedAuthorId != ui.item.id) {
+        lastSelectedAuthorId = ui.item.id;
+        authorSelectHandler(event, ui);
+      } 
     });
 
     setSelectsEvents();
