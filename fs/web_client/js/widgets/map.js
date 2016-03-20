3a7261a618e4e45d6e9efc831a91a11a652671a3
quasilyte <quasilyte@gmail.com>
2016-03-21 00:50:42 +0300

    виджет яндекс карты

---
+++ fs/web_client/js/widgets/map.js
@@ -0,0 +1,88 @@
+'use strict';
+
+App.widgets.Map = function(params, id) {
+  var tmpl =  _.template(`
+    <div id="<%= id %>" style="width: 100%; height: 100%"></div>
+  `);
+                         
+  var geo = {
+    'kazan': [55.78, 49.13],
+    'moscow': [55.76, 37.64]
+  };
+  var placemarks = {};
+  var map = null;
+  var eventsPool = {};
+
+  params = _.extend({
+    'center': geo.kazan,
+    'zoom': 7,
+    'controls': [
+      'zoomControl',
+      'searchControl',
+      'typeSelector',
+      'rulerControl'
+    ]
+  }, params);
+  
+  this.early = () => tmpl({'id': id});
+  this.later = () => ymaps.ready(delayedLater);
+
+  function delayedLater() {
+    map = new ymaps.Map(id, {
+      'center': params.center,
+      'zoom': params.zoom,
+      'controls': params.controls
+    });
+
+    map.behaviors.disable('dblClickZoom');
+
+    // Включаем отложенные подписки на события.
+    _.each(eventsPool, function(callback, event) {
+      map.events.add(event, callback);
+    });
+    eventsPool = null; // Больше нам не нужно их держать.
+  }
+
+  this.center = function(coord, opts) {
+    map.setCenter(coord, map.getZoom(), opts);
+  };
+
+  this.removePlacemark = function(id) {
+    if (placemarks[id]) {
+      map.geoObjects.remove(placemarks[id])
+      delete placemarks[id];
+    }
+  };
+
+  this.addPlacemark = function(coord, id) {
+    var placemark = new ymaps.Placemark(
+      coord, 
+      {}, // Данные внутри отметки
+      {'preset': 'islands#darkGreenCircleDotIcon'} // Стилизация отметки
+    );
+
+    if (id) {
+      placemarks[id] = placemark;
+    }
+
+    map.geoObjects.add(placemark);
+  };
+
+  this.updatePlacemark = function(id, coord) {
+    if (placemarks[id]) {
+      placemarks[id].geometry.setCoordinates(coord);
+    } else {
+      this.addPlacemark(coord, id);
+    }
+  };
+
+  this.on = function(event, callback) {
+    if (map) {
+      map.events.add(event, callback);
+    } else {
+      // Не можем навесить событие до того, как будет создана карта.
+      // Кладём подписку в очередь, биндить их будем позже.
+      eventsPool[event] = callback;
+    }
+  };
+};
