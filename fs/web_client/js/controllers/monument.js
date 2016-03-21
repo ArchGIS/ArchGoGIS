aefb4ff5483551468cad335d4fa3fc449bba2cdc
quasilyte <quasilyte@gmail.com>
2016-03-21 12:14:47 +0300

    починил страницу памятника (контроллер monument)@

--- fs/web_client/js/controllers/monument.js
+++ fs/web_client/js/controllers/monument.js
@@ -2,18 +2,19 @@
 
 App.controllers.monument = new (App.View.extend({
   'show': function() {
-    var id = App.url.get("id");
-
+    App.url.setMapping(['id']);
+    var id = App.url.get('id');
+    
     var query = {
       "m:Monument": {"id": id, "select": "*"},
       "k:Knowledge": {"id": "*", "select": "*"},
       "r:Research": {"id": "*", "select": "*"},
       "a:Author": {"id": "*", "select": "*"},
-      "o:Object": {"id": "*", "select": "*"},
+      "?o:Object": {"id": "*", "select": "*"},
       "k_Describes_m": {},
       "r_Contains_k": {},
       "a_Created_r": {},
-      "m_Contains_o": {}
+      "?m_Contains_o": {}
     };
     $.post('/hquery/read', JSON.stringify(query))
     .success(function(response) {
