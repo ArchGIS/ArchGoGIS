35f047aa296c1ee65ed1732e851ce2ee67db8ddb
quasilyte <quasilyte@gmail.com>
2016-03-23 18:25:04 +0300

    набросочное отображение исследований

--- fs/web_client/js/controllers/research.js
+++ fs/web_client/js/controllers/research.js
@@ -7,10 +7,31 @@ App.controllers.research = new (App.View.extend({
 
   'show': function() {
     App.url.setMapping(['id']);
-    
     var id = App.url.get("id");
 
-    App.page.render('research_view', {"id": id});
-    console.log('research view');
+    var query = JSON.stringify({
+      "r:Research": {"id": id, "select": "*"},
+      "k:Knowledge": {"id": "*", "select": "*"},
+      "m:Monument": {"id": "*", "select": "*"},
+      "r_Contains_k": {},
+      "k_Describes_m": {}
+    });
+
+    $.post('/hquery/read?limit=400', query)
+    .success(function(researchData) {
+      researchData = JSON.parse(researchData);
+
+      var query = JSON.stringify({
+        "r:Research": {"id": id},
+        "a:Author": {"id": "?", "select": "*"},
+        "a_Created_r": {}
+      });
+
+      $.post('/hquery/read', query)
+      .success(function(authorData) {
+        authorData = JSON.parse(authorData);
+        App.page.render('research_view', $.extend(researchData, authorData));
+      });
+    });
   }
 }));
