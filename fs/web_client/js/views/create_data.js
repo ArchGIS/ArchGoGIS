a627e22f8be091ffced994bd77299bff5c5e1d90
quasilyte <quasilyte@gmail.com>
2016-04-15 15:56:07 +0300

    отрефакторил страницу wizard'а данных

--- fs/web_client/js/views/create_data.js
+++ fs/web_client/js/views/create_data.js
@@ -2,16 +2,36 @@
 
 App.views.wizard = new (App.View.extend({
   'create_data': function() {
+    var $sources = $("#from-what-create");
+    var t = App.locale.translate;
+
+    $("#goto-create").on("click", function() {
+      var $selectedSource = $sources.find(":selected");
+      
+      if ($selectedSource.text()) {
+        window.location.href = $selectedSource.attr("value");
+      }
+    });
+
+    var options = {
+      "monument": {
+        "#monument/new": t("report.singular")
+      },
+      "monuments": {
+        "#monument/new_by_arch_map": t("arch-map.singular")
+      },
+      "artifact": {
+        "#artifact/new": t("research.singular")
+      }
+    };
+    
     $("#what-create").on('change', function(obj) {
       var val = $("#what-create").val();
-      if (val == "monument") {
-        $("#from-what-create").html("<option value='report'>Отчет</option>");
-        $("#goto-create").attr("href", "#monument/new");
-      } else if (val == "artifact") {
-        $("#from-what-create").html("<option value='research'>Исследование</option>");
-        $("#goto-create").attr("href", "#artifact/new");
-      }
-      console.log($("#what-create").val())
+      
+      $sources.empty();
+      $.each(options[val], function(href, text) {
+        $sources.append($("<option/>").attr("value", href).text(text));
+      });
     })
   }
 }));
