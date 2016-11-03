b559978463489bcbb901159128b901803484809c
Maxim <kalhior@yandex.ru>
2016-11-03 14:32:16 +0300

    Страница публикации

--- fs/web_client/js/controllers/publication.js
+++ fs/web_client/js/controllers/publication.js
@@ -4,70 +4,53 @@ App.controllers.publication = new (App.View.extend({
   "show": function() {
   	App.url.setMapping(["id"]);
     var pid = App.url.get("id");
-    var tmplData = {
-      "publisher": [],
-      "pubtype": [],
-      "author": [],
-      "coauthors": [],
-    };
+    var data = [];
+    var tmplData = {};
+    var model = App.models.fn;
 
-    var d1 = $.Deferred(),
-        d2 = $.Deferred(),
-        d3 = $.Deferred(),
-        d4 = $.Deferred(),
-        d5 = $.Deferred();
+    var queries = {
+      single: {
+        publication: JSON.stringify({
+          "pub:Publication": {"id": pid, "select": "*"},
+        }),
+        pubType: JSON.stringify({
+          "pub:Publication": {"id": pid},
+          "pubtype:PublicationType": {"id": "*", "select": "*"},
+          "pub_has_pubtype": {},
+        }),
+        publisher: JSON.stringify({
+          "pub:Publication": {"id": pid},
+          "publisher:Publisher" :  {"id": "*", "select": "*"},
+          "publisher_has_pub": {},
+        }),
+        authors: JSON.stringify({
+          "pub:Publication": {"id": pid},
+          "author:Author": {"id": "*", "select": "*"},
+          "pub_hasauthor_author": {},
+        }),
+        coauthors: JSON.stringify({
+          "pub:Publication": {"id": pid},
+          "coauthors:Author": {"id": "*", "select": "*"},
+          "pub_hascoauthor_coauthors": {},
+        })
+      }
+    }
 
-    var query_publication = JSON.stringify({
-      "pub:Publication": {"id": pid, "select": "*"},
-    });
+     var render = function() {
+      _.each(data, function(val, id) {
+        _.extend(tmplData, val);
+      })
+      console.log(tmplData);
 
-    var query_pub_type = JSON.stringify({
-      "pub:Publication": {"id": pid},
-      "pubtype:PublicationType": {"id": "*", "select": "*"},
-      "pub_has_pubtype": {},
-    });
-
-    var query_publisher = JSON.stringify({
-      "pub:Publication": {"id": pid},
-      "publisher:Publisher" :  {"id": "*", "select": "*"},
-      "publisher_has_pub": {},
-    });
-
-    var query_authors = JSON.stringify({
-      "pub:Publication": {"id": pid},
-			"author:Author": {"id": "*", "select": "*"},
-      "pub_hasauthor_author": {},
-    });
-
-    var query_coauthors = JSON.stringify({
-      "pub:Publication": {"id": pid},
-			"coauthors:Author": {"id": "*", "select": "*"},
-      "pub_hascoauthor_coauthors": {},
-    });
-
-		$.when(App.models.fn.sendQueryWithDeferred(query_publication, d1)).then(function(response) {
-      _.extend(tmplData, response);
-    })
-
-    $.when(App.models.fn.sendQueryWithDeferred(query_pub_type, d2)).then(function(response) {
-      _.extend(tmplData, response);
-    })
-
-    $.when(App.models.fn.sendQueryWithDeferred(query_publisher, d3)).then(function(response) {
-      _.extend(tmplData, response);
-    })
-
-    $.when(App.models.fn.sendQueryWithDeferred(query_authors, d4)).then(function(response) {
-      _.extend(tmplData, response);
-    })
-
-    $.when(App.models.fn.sendQueryWithDeferred(query_coauthors, d5)).then(function(response) {
-      _.extend(tmplData, response);
-    })
-
-		console.log(tmplData);
-    $.when(d1, d2, d3, d4, d5).done(function() {
       App.page.render("publication/show", tmplData);
-    });
+    }
+
+    var queryCounter = _.reduce(queries, function(memo, obj) {
+      return memo + _.size(obj)
+    }, 0)
+
+    var callRender = _.after(queryCounter, render);
+
+    data.push(model.getData(queries.single, callRender));
   }
 }));
