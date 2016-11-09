df71dd80eb401b2395996b81a14af9dce2222639
bontequero <bontequero@gmail.com>
2016-11-09 19:39:19 +0300

    удалил временные сущности в базе; добавил дроп секвенсов в постгресе

--- db/postgres/0_sequences.sql
+++ db/postgres/0_sequences.sql
@@ -1,3 +1,25 @@
+DROP SEQUENCE n_author_id_seq;
+DROP SEQUENCE n_monument_id_seq;
+DROP SEQUENCE n_knowledge_id_seq;
+DROP SEQUENCE n_research_id_seq;
+DROP SEQUENCE n_geometry_id_seq;
+DROP SEQUENCE n_object_id_seq;
+DROP SEQUENCE n_artifact_id_seq;
+DROP SEQUENCE n_organization_seq;
+DROP SEQUENCE n_report_id_seq;
+DROP SEQUENCE n_file_id_seq;
+DROP SEQUENCE n_monument_analysis_id_seq;
+DROP SEQUENCE n_heritage_status_id_seq;
+DROP SEQUENCE n_epoch_id_seq;
+DROP SEQUENCE n_culture_id_seq;
+DROP SEQUENCE n_research_type_id_seq;
+DROP SEQUENCE n_complex_id_seq;
+DROP SEQUENCE n_image_id_seq;
+DROP SEQUENCE n_city_id_seq;
+DROP SEQUENCE n_author_job_id_seq;
+DROP SEQUENCE n_excavation_id_seq;
+DROP SEQUENCE local_storage_file_id_seq;
+
 CREATE SEQUENCE n_author_id_seq;
 CREATE SEQUENCE n_monument_id_seq;
 CREATE SEQUENCE n_knowledge_id_seq;
