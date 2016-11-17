d7bea8fd2e579b1f705cf25eea97f520690cf75d
Maxim <kalhior@yandex.ru>
2016-11-17 14:52:55 +0300

    Новые вкладки в верхнем меню, немного поменял создание ОКН

--- fs/web_client/js/controllers/wizard.js
+++ fs/web_client/js/controllers/wizard.js
@@ -3,5 +3,29 @@
 App.controllers.wizard = new (Backbone.View.extend({
   'create_data': function() {
     App.page.render('wizard/create_data');
-  }
+  },
+
+  'future1': function() {
+    App.page.render('wizard/future1');
+  },
+
+  'future2': function() {
+    App.page.render('wizard/future2');
+  },
+
+  'future3': function() {
+    App.page.render('wizard/future3');
+  },
+
+  'future4': function() {
+    App.page.render('wizard/future4');
+  },
+
+  'future5': function() {
+    App.page.render('wizard/future5');
+  },
+
+  'future6': function() {
+    App.page.render('wizard/future6');
+  },
 }));
