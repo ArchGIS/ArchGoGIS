3001612d43df9608bcea634c1285b512c45c9709
bontequero <bontequero@gmail.com>
2016-11-21 10:57:36 +0300

    go way archmap

--- service/search/arch_map_handler.go
+++ service/search/arch_map_handler.go
@@ -1,18 +1,19 @@
 package search
 
 import (
-	"github.com/ArchGIS/ArchGoGIS/db/neo"
 	"errors"
-	"github.com/ArchGIS/ArchGoGIS/ext"
 	"net/http"
 	"regexp"
+
+	"github.com/ArchGIS/ArchGoGIS/db/neo"
+	"github.com/ArchGIS/ArchGoGIS/ext"
 	"github.com/ArchGIS/ArchGoGIS/service/search/errs"
 	"github.com/ArchGIS/ArchGoGIS/web"
 	"github.com/ArchGIS/ArchGoGIS/web/api"
 )
 
 var (
-	searchPatternError = errors.New("1000")
+	errSearchPattern = errors.New("1000")
 )
 
 var (
@@ -21,6 +22,7 @@ var (
 
 func archMapHandler(w web.ResponseWriter, r *http.Request) {
 	result, err := searchForArchMap(r.URL.Query().Get("term"))
+
 	if err == nil {
 		w.Write(result)
 	} else {
@@ -31,7 +33,7 @@ func archMapHandler(w web.ResponseWriter, r *http.Request) {
 func searchForArchMap(term string) ([]byte, error) {
 	matches := matcher.FindStringSubmatch(term)
 	if len(matches) < 2 || len(matches) > 4 {
-		return nil, searchPatternError
+		return nil, errSearchPattern
 	}
 
 	if 4 == len(matches) && matches[3] != "" {
@@ -40,54 +42,59 @@ func searchForArchMap(term string) ([]byte, error) {
 			"MATCH (p:Publisher)-[:Published {year: {publicationYear}}]->(am)" +
 			"WHERE au.name STARTS WITH {authorName}" +
 			"RETURN am"
+
 		resp, err := neo.Run(query, neo.Params{
 			"authorName":      `"` + matches[1] + `"`,
 			"publicationYear": matches[3],
 		})
+
 		if err != nil {
 			return nil, errs.RetrieveError
 		}
+
 		if len(resp.Results[0].Data) == 0 {
 			return []byte("[]"), nil
-		} else {
-			var buf ext.Xbuf
-
-			buf.WriteByte('[')
-			for _, row := range resp.Results[0].Data {
-				buf.Write(row.Row[0])
-				buf.WriteByte(',')
-			}
-			buf.DropLastByte()
-			buf.WriteByte(']')
-
-			return buf.Bytes(), nil
 		}
-	} else {
-		const query = "MATCH (am:ArchMap)" +
-			"MATCH (au:Author)-[:Created]->(am)" +
-			"WHERE au.name STARTS WITH {authorName}" +
-			"RETURN am"
-		resp, err := neo.Run(query, neo.Params{
-			"authorName": `"` + matches[1] + `"`,
-		})
-		if err != nil {
-			return nil, errs.RetrieveError
-		}
-		if len(resp.Results[0].Data) == 0 {
-			return []byte("[]"), nil
-		} else {
-			var buf ext.Xbuf
 
-			buf.WriteByte('[')
-			for _, row := range resp.Results[0].Data {
-				buf.Write(row.Row[0])
-				buf.WriteByte(',')
-			}
-			buf.DropLastByte()
-			buf.WriteByte(']')
+		var buf ext.Xbuf
 
-			return buf.Bytes(), nil
+		buf.WriteByte('[')
+		for _, row := range resp.Results[0].Data {
+			buf.Write(row.Row[0])
+			buf.WriteByte(',')
 		}
+		buf.DropLastByte()
+		buf.WriteByte(']')
+
+		return buf.Bytes(), nil
 	}
 
+	const query = "MATCH (am:ArchMap)" +
+		"MATCH (au:Author)-[:Created]->(am)" +
+		"WHERE au.name STARTS WITH {authorName}" +
+		"RETURN am"
+
+	resp, err := neo.Run(query, neo.Params{
+		"authorName": `"` + matches[1] + `"`,
+	})
+
+	if err != nil {
+		return nil, errs.RetrieveError
+	}
+
+	if len(resp.Results[0].Data) == 0 {
+		return []byte("[]"), nil
+	}
+	
+	var buf ext.Xbuf
+
+	buf.WriteByte('[')
+	for _, row := range resp.Results[0].Data {
+		buf.Write(row.Row[0])
+		buf.WriteByte(',')
+	}
+	buf.DropLastByte()
+	buf.WriteByte(']')
+
+	return buf.Bytes(), nil
 }
