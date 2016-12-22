a49c3c9738d776da6bee87dc27fa1db295c7060f
Maxim <kalhior@yandex.ru>
2016-12-22 11:05:23 +0300

    Неиспользуемые файлы на сервере

--- service/hquery/read/ast/node.go
+++ service/hquery/read/ast/node.go
@@ -1,17 +1,15 @@
 package ast
 
 import (
-	"github.com/ArchGIS/ArchGoGIS/service/hquery/errs"
 	"github.com/ArchGIS/ArchGoGIS/service/hquery/parsing"
-	"github.com/ArchGIS/ArchGoGIS/throw"
 )
 
 func MustNewNode(tag string, query map[string]string) *Node {
-	if (query["id"]) {
+	if (query["id"] != "") {
 		mustValidateMatcher(query["id"])
 	}
 
-	if (query["select"]) {
+	if (query["select"] != "") {
 		mustValidateSelector(query["select"])
 	}
 
