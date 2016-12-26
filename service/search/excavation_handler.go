4d9efca6e451438bef2a502aedbed2b145683573
Maxim <kalhior@yandex.ru>
2016-12-26 08:12:13 +0300

    Добавил страницу для вскрытий. Также их теперь можно искать в быстром поиске4

---
+++ service/search/excavation_handler.go
@@ -0,0 +1,86 @@
+package search
+
+import (
+	"bytes"
+	"unsafe"
+	"net/http"
+
+	"github.com/ArchGIS/ArchGoGIS/db/neo"
+	"github.com/ArchGIS/ArchGoGIS/echo"
+	"github.com/ArchGIS/ArchGoGIS/ext"
+	"github.com/ArchGIS/ArchGoGIS/service/search/errs"
+	"github.com/ArchGIS/ArchGoGIS/web"
+	"github.com/ArchGIS/ArchGoGIS/web/api"
+)
+
+const (
+	filterExcavationCypher = "MATCH (r:Excavation)" + 
+		"MATCH (a:Author)--(res:Research)--(r)"
+)
+
+
+func filterExcavationHandler(w web.ResponseWriter, r *http.Request) {
+	result, err := searchForFilterExcavation(
+		r.URL.Query().Get("author"),
+		r.URL.Query().Get("year"))
+
+
+	if err == nil {
+		w.Write(result)
+	} else {
+		w.Write(api.Error(err))
+	}
+}
+
+func searchForFilterExcavation(author, year string) ([]byte, error) {
+	params := neo.Params{}
+	query := filterExcavationCypher + "WHERE "
+	first := false
+
+	if author != "" {
+		query = query + "a.name =~ {author} "
+		first = true
+		params["author"] = `"(?ui)^.*` + author + `.*$"`
+	}
+	if year != "" {
+		if first {
+			query = query + "AND res.year = {year} "
+		} else {
+			query = query + "res.year = {year} "
+			// first = true
+		}
+
+		params["year"] = year
+	}
+	
+	query = query + "RETURN r.id, r.name, res.year, a.name"
+
+	resp, err := neo.Run(query, params)
+
+	if err != nil {
+		echo.ServerError.Print(err)
+		return nil, errs.RetrieveError
+	}
+
+	if len(resp.Results[0].Data) == 0 {
+		return []byte("[]"), nil
+	}
+
+	// Подготавливаем ответ.
+	var buf ext.Xbuf
+
+	buf.WriteByte('[')
+	for _, row := range resp.Results[0].Data {
+		// #FIXME: перепиши меня, когда будет время!
+		buf.WriteByte('[')
+		buf.Write(
+			bytes.Join(*(*[][]byte)(unsafe.Pointer(&row.Row)), []byte(",")),
+		)
+		buf.WriteByte(']')
+		buf.WriteByte(',')
+	}
+	buf.DropLastByte()
+	buf.WriteByte(']')
+
+	return buf.Bytes(), nil
+}
