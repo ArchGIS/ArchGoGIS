95fc6e4b2a6558ed7bb8f4dbc143231f60a59b0b
bontequero <bontequero@gmail.com>
2017-02-16 13:12:31 +0300

    Вынес шаблон и увеличил значки исследований

--- fs/web_client/js/views/heritage.js
+++ fs/web_client/js/views/heritage.js
@@ -233,41 +233,7 @@ App.views.heritage = new (Backbone.View.extend({
   },
 
   'show': function(arg) {
-    const types       = _.uniq( _.pluck(arg.placemarks, 'type') ),
-          mapInstance = App.views.map(types),
-          map         = mapInstance.map,
-          overlays    = mapInstance.overlayLayers;
-
-    _.each(arg.placemarks, function(item) {
-      if (!item.coords[0] && !item.coords[1]) { return; }
-      
-      const pathToIcon = `/web_client/img/${App.store.pathToIcons[item.type]}`;
-      const icon = L.icon({
-        iconUrl: `${pathToIcon}/${item.opts.preset}.png`,
-        iconSize: [16, 16]
-      });
-
-      let marker = L.marker(L.latLng(item.coords[0], item.coords[1]), {
-        icon: icon
-      });
-
-      marker.bindTooltip(item.pref.hintContent, {
-        direction: 'top'
-      });
-
-      marker.on('mouseover', function(e) {
-        this.openTooltip();
-      });
-      marker.on('mouseout', function(e) {
-        this.closeTooltip();
-      });
-      marker.on('click', function(e) {
-        location.hash = `${item.type}/show/${item.id}`
-      });
-
-      overlays[App.store.mapTypes[item.type]].addLayer(marker);
-    });
-
+    App.views.addToMap(arg.placemarks);
     
     console.log(arg)
     _.each(arg.stateTables, function(table, id) {
--- fs/web_client/js/views/map.js
+++ fs/web_client/js/views/map.js
@@ -97,5 +97,51 @@ App.views.map = (types) => {
     map,
     overlayLayers
   }
-}
+};
 
+App.views.addToMap = (placemarks) => {
+  const types       = _.uniq( _.pluck(placemarks, 'type') ),
+        mapInstance = App.views.map(types),
+        map         = mapInstance.map,
+        overlays    = mapInstance.overlayLayers;
+
+  const icon16 = [16, 16],
+        icon24 = [24, 24];
+  const iconSizes = {
+    research: icon24,
+    monument: icon16,
+    artifact: icon16,
+    heritage: icon16,
+    excavation: icon16
+  }
+
+  _.each(placemarks, function(item) {
+    if (!item.coords[0] && !item.coords[1]) { return; }
+    
+    const pathToIcon = `/web_client/img/${App.store.pathToIcons[item.type]}`;
+    const icon = L.icon({
+      iconUrl: `${pathToIcon}/${item.opts.preset}.png`,
+      iconSize: iconSizes[`${item.type}`]
+    });
+
+    let marker = L.marker(L.latLng(item.coords[0], item.coords[1]), {
+      icon: icon
+    });
+
+    marker.bindTooltip(item.pref.hintContent, {
+      direction: 'top'
+    });
+
+    marker.on('mouseover', function(e) {
+      this.openTooltip();
+    });
+    marker.on('mouseout', function(e) {
+      this.closeTooltip();
+    });
+    marker.on('click', function(e) {
+      location.hash = `${item.type}/show/${item.id}`
+    });
+
+    overlays[App.store.mapTypes[item.type]].addLayer(marker);
+  });
+};
