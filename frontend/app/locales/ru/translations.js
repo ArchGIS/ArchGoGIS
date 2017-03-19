b512abe6135e10537d9d7ffe7257af2cfed66198
Максим Ефимов < >
2017-03-19 16:44:25 +0300

    Запилил locales

---
+++ frontend/app/locales/ru/translations.js
@@ -0,0 +1,12 @@
+export default {
+  'navbar': {
+    'graph-builder': 'Создание данных',
+    'map-builder': 'Создание карты',
+    'table-loader': 'Создание выборки',
+    'vector-uploader': 'Загрузка данных',
+    'big-search': 'Расширенный поиск',
+    'data-builder': 'Внесение данных',
+    'fast-search': 'Быстрый поиск',
+    'admin-panel': 'Административная панель',
+  },
+};
