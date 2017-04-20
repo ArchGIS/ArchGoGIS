efda2ab99b2cfaff9755482ad55f838c97269ca0
Максим Ефимов < >
2017-04-20 17:01:57 +0300

    Увеличил лимит возвращаемых данных

--- cfg/hquery.go
+++ cfg/hquery.go
@@ -8,7 +8,7 @@ const (
 	HqueryUpsertMaxInputLen = 20048
 	HqueryReadMaxInputLen   = 1024
 	HqueryReadDefaultLimit  = "500"
-	HqueryReadMaxLimit      = "1500"
+	HqueryReadMaxLimit      = "15000"
 	HqueryMaxEntries        = 32
 	HqueryMaxPropsPerEntry  = 24
 	HqueryMaxPropsTotal     = 1200
