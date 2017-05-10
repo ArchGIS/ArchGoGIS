270cce9b7d7bec6098c1c54a61a28d7b4b166c11
Максим Ефимов < >
2017-05-10 08:35:00 +0300

    Убрал mon layers

--- fs/web_client/js/init/app.js
+++ fs/web_client/js/init/app.js
@@ -93,7 +93,6 @@ function postQuery(objectId) {
     ["Monument", "Epoch", "has"],
     ["Monument", "MonumentType", "has"],
     ["Monument", "SpatialReference", "has"],
-    ["MonumentLayers", "Monument", "has"],
     ["ClarifyMon", "SpatialReference", "has"],
     ["SpatialReference", "SpatialReferenceType", "has"],
     ["Knowledge", "monumentImage", "has"],
