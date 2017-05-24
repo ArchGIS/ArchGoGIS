36f1f47f33b1c747ce1d16ab183b079f6314f71b
Делюс Фархуллин <bontequero@gmail.com>
2017-05-24 07:42:26 +0300

    WIP кластеры

--- fs/web_client/js/views/map.js
+++ fs/web_client/js/views/map.js
@@ -119,11 +119,18 @@ const resTypeHash = [
 ];
 
 App.views.createOverlays = (leaf, types) => {
+  const rmax = 20;
+
   const cluster = L.markerClusterGroup({
-    maxClusterRadius: 40,
+    maxClusterRadius: rmax * 2,
     iconCreateFunction: (cluster) => {
-      const markers = cluster.getAllChildMarkers();
-      const markerTypes = [];
+      const markers = cluster.getAllChildMarkers(),
+            n = markers.length,
+            markerTypes = [],
+            strokeWidth = 1,
+            r = rmax - 2 * strokeWidth - (n < 10 ? 12 : n < 100 ? 8 : n < 1000 ? 4 : 0),
+            iconDim = (r + strokeWidth) * 2;
+
       let type = '';
 
       for (let marker of markers) {
@@ -132,7 +139,7 @@ App.views.createOverlays = (leaf, types) => {
         }
 
         if (markerTypes.length > 1) {
-          type = setDefaultClassToMarkerCluster(markers.length);
+          type = setDefaultClassToMarkerCluster(n);
           break;
         }
       };
@@ -140,19 +147,42 @@ App.views.createOverlays = (leaf, types) => {
       if (type === '') {
         if (markerTypes[0] === 'monument') {
           type = 'marker-cluster cluster-monument';
+
+          const data = d3.nest()
+            .key(d => d.epoch)
+            .entries(markers, d3.map);
+
+          const html = bakeThePie({
+            data: data,
+            valueFunc: d => d.values.length,
+            strokeWidth: 1,
+            outerRadius: r,
+            innerRadius: r - 10,
+            pieClass: 'cluster-pie',
+            pieLabel: n,
+            pieLabelClass: 'marker-cluster-pie-label',
+            pathClassFunc: d => "category-" + d.data.key,
+            pathTitleFunc: d => metadata.fields[categoryField].lookup[d.data.key] + ' (' + d.data.values.length + ' accident' + (d.data.values.length!=1?'s':'')+')'
+          });
+
+          return L.divIcon({
+            html: html,
+            className: 'marker-cluster',
+            iconSize: L.point(iconDim, iconDim)
+          });
         } else if (markerTypes[0] === 'research') {
           type = 'marker-cluster cluster-research';
         } else if (markerTypes[0] === 'radiocarbon') {
           type = 'marker-cluster cluster-radiocarbon';
         } else {
-          type = setDefaultClassToMarkerCluster(markers.length);
+          type = setDefaultClassToMarkerCluster(n);
         }
       }
 
       return L.divIcon({
-        html: '<div><span>' + markers.length + '</span></div>',
+        html: '<div><span>' + n + '</span></div>',
         className: type,
-        iconSize: L.point(40, 40)
+        iconSize: L.point(iconDim, iconDim)
       });
     }
   });
@@ -248,3 +278,72 @@ App.views.clearOverlays = (leaf) => {
     });
   }
 };
+
+
+
+function bakeThePie(options) {
+    /*data and valueFunc are required*/
+    if (!options.data || !options.valueFunc) {
+        return '';
+    }
+    var data = options.data,
+        valueFunc = options.valueFunc,
+        r = options.outerRadius?options.outerRadius:28, //Default outer radius = 28px
+        rInner = options.innerRadius?options.innerRadius:r-10, //Default inner radius = r-10
+        strokeWidth = options.strokeWidth?options.strokeWidth:1, //Default stroke is 1
+        pathClassFunc = options.pathClassFunc?options.pathClassFunc:function(){return '';}, //Class for each path
+        pathTitleFunc = options.pathTitleFunc?options.pathTitleFunc:function(){return '';}, //Title for each path
+        pieClass = options.pieClass?options.pieClass:'marker-cluster-pie', //Class for the whole pie
+        pieLabel = options.pieLabel?options.pieLabel:d3.sum(data,valueFunc), //Label for the whole pie
+        pieLabelClass = options.pieLabelClass?options.pieLabelClass:'marker-cluster-pie-label',//Class for the pie label
+        
+        origo = (r+strokeWidth), //Center coordinate
+        w = origo*2, //width and height of the svg element
+        h = w,
+        donut = d3.layout.pie(),
+        arc = d3.svg.arc().innerRadius(rInner).outerRadius(r);
+        
+    //Create an svg element
+    var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');
+    //Create the pie chart
+    var vis = d3.select(svg)
+        .data([data])
+        .attr('class', pieClass)
+        .attr('width', w)
+        .attr('height', h);
+        
+    var arcs = vis.selectAll('g.arc')
+        .data(donut.value(valueFunc))
+        .enter().append('svg:g')
+        .attr('class', 'arc')
+        .attr('transform', 'translate(' + origo + ',' + origo + ')');
+    
+    arcs.append('svg:path')
+        .attr('class', pathClassFunc)
+        .attr('stroke-width', strokeWidth)
+        .attr('d', arc)
+        .append('svg:title')
+          .text(pathTitleFunc);
+                
+    vis.append('text')
+        .attr('x',origo)
+        .attr('y',origo)
+        .attr('class', pieLabelClass)
+        .attr('text-anchor', 'middle')
+        //.attr('dominant-baseline', 'central')
+        /*IE doesn't seem to support dominant-baseline, but setting dy to .3em does the trick*/
+        .attr('dy','.3em')
+        .text(pieLabel);
+    //Return the svg-markup rather than the actual element
+    return serializeXmlNode(svg);
+}
+
+
+function serializeXmlNode(xmlNode) {
+    if (typeof window.XMLSerializer != "undefined") {
+        return (new window.XMLSerializer()).serializeToString(xmlNode);
+    } else if (typeof xmlNode.xml != "undefined") {
+        return xmlNode.xml;
+    }
+    return "";
+}
