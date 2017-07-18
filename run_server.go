1cbd90599c91f45189761ed4a0adc6b93788eefb
Делюс Фархуллин <bontequero@gmail.com>
2017-07-18 11:31:56 +0300

    Сделал обработку OPTIONS для всех запросов

--- run_server.go
+++ run_server.go
@@ -37,6 +37,7 @@ func main() {
 	e.Logger.SetLevel(log.DEBUG)
 	e.Use(middleware.Recover())
 	e.Use(addOptions())
+	e.Use(handleOptions())
 
 	e.Static("/vendor", "fs/vendor")
 	e.Static("/local_storage", "fs/local_storage")
@@ -73,6 +74,22 @@ func addOptions() echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			origin := c.Request().Header.Get("Origin")
 			c.Response().Header().Set("Access-Control-Allow-Origin", origin)
+			return next(c)
+		}
+	}
+}
+
+func handleOptions() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if c.Request().Method == "OPTIONS" {
+				c.Response().Header().Set("Allow", "OPTIONS, GET, POST")
+				origin := c.Request().Header.Get("Origin")
+				c.Response().Header().Set("Access-Control-Allow-Origin", origin)
+				c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type")
+				return next(c)
+			}
+
 			return next(c)
 		}
 	}
