3ee257ee4eb18ea7f0f7cc5a6bff20550859f0a4
KpekepRu <kalhior@yandex.ru>
2018-02-28 17:31:20 +0300

    Добавил выгрузку координат в выборке

--- fs/web_client/js/views/selection.js
+++ fs/web_client/js/views/selection.js
@@ -93,5 +93,109 @@ App.views.selection = new (Backbone.View.extend({
       var ids = _.map(data.monuments, function(obj) {return obj.id.toString()});
       carbons = model.getData(query, callGenerate, true, ids);
     }) 
+
+    $("#get-sites-coords").on("click", function() {
+      var model = App.models.fn;
+      var sites = [];
+
+      var queries = {
+        main: JSON.stringify({
+          "mons:Monument": {"id": "NEED"},
+          "e:Epoch": {"id": "*", "select": "*"},
+          "mt:MonumentType": {"id": "*", "select": "*"},
+          "mons__e": {},
+          "mons__mt": {},
+        }),
+
+        cults: JSON.stringify({
+          "mons:Monument": {"id": "NEED"},
+          "knows:Knowledge": {"id": "*", "select": "*"},
+          "cults:Culture": {"id": "*", "select": "*"},
+          "mons__knows": {},
+          "knows__cults": {},
+        }),
+
+        coords: JSON.stringify({
+          "mons:Monument": {"id": "NEED"},
+          "sp:SpatialReference": {"id": "*", "select": "*"},
+          "spt:SpatialReferenceType": {"id": "*", "select": "*"},
+          "mons__sp": {},
+          "spt__sp": {},
+        }),
+      }
+
+      var generate = function() {
+        console.log(sites)
+        var type = 'data:application/octet-stream;base64, ';
+        var text = "", line;
+        var names=[], cults=[], coords=[], epochs=[], mtypes=[];
+        var count, name, mtype, epoch, cult, coord;
+
+        _.each(sites.main, function(obj, i) {
+          epochs[i] = [obj.e[0].name]; 
+          mtypes[i] = [obj.mt[0].name]; 
+          names[i] = _.uniq(_.pluck(sites.cults[i].knows, "name"));
+          cults[i] = _.uniq(_.pluck(sites.cults[i].cults, "name"));
+          coords[i] = App.fn.findActualSpatref(sites.coords[i].sp, sites.coords[i].spt);
+        })
+
+        normalizeStringArray(epochs)
+        normalizeStringArray(mtypes)
+        normalizeStringArray(names)
+        normalizeStringArray(cults)
+
+        _.each(names, function(obj, i) {
+          count = Math.max(names[i].length, cults[i].length, 2)
+          console.log(coords)
+          for (var t=0; t<count; t++) {
+            epoch = epochs[i][t] || " ".repeat(epochs[i][0].length - 3) + " | "
+            mtype = mtypes[i][t] || " ".repeat(mtypes[i][0].length - 3) + " | "
+            name = names[i][t] || " ".repeat(names[i][0].length - 3) + " | "
+            cult = cults[i][t] || " ".repeat(cults[i][0].length - 3) + " | "
+            
+            coord = (t == 0) ? `N: ${coords[i].x}` : `E: ${coords[i].y}`
+            if (t > 1) 
+              coord = ""
+
+            line = `${name} ${mtype} ${epoch} ${cult} ${coord}\n`;
+            text += `${line}`;
+          }
+
+          text += "-".repeat(line.length) + `\n`;
+        })
+
+        var base = window.btoa(unescape(encodeURIComponent(text)));
+        var res = type + base;
+        $('#test').attr('href', res);
+        $('#test').attr('download', 'sites_coordinates.txt');
+        $('#test').show();
+      };
+
+      var queryCounter = _.size(queries);
+      var callGenerate = _.after(queryCounter, generate);
+
+      var ids = _.map(data.monuments, function(obj) {return obj.id.toString()});
+      sites = model.getData(queries, callGenerate, true, ids);
+    })
+
+    var normalizeStringArray = function(array) {
+      var maxLength = 0;
+
+      _.each(array, function(str, i) {
+        _.each(str, function(s, t) {
+          if (s.length > maxLength) 
+            maxLength = s.length;
+        })
+      })
+
+      maxLength += 1;
+
+      _.each(array, function(strings, i) {
+        _.each(strings, function(str, t) {
+          if (str.length < maxLength) 
+            array[i][t] += " ".repeat(maxLength - str.length) + " | ";
+        })
+      })
+    }  
   },
 }))
