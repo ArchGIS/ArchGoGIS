c54a4b470897ad6d13cf7e19d85c82725d38ed39
KpekepRu <kalhior@yandex.ru>
2018-04-14 18:44:26 +0300

    Добавил кнопку загрузки файлов в отчетах

--- fs/web_client/js/views/report.js
+++ fs/web_client/js/views/report.js
@@ -1,10 +1,45 @@
 'use strict';
 
 App.views.report = new (Backbone.View.extend({
-  "show": function(argument) {
+  "show": function(arg, arg2) {
+    let repId = App.url.get('id');
+
     $(".view-icon").on('click', function() {
       let fileid = $(this).attr('fileid');
       $("#report-view-file").attr('src', (App.models.File.url(fileid)));
     }, this)
+
+    let filesId = $.map($(".view-icon"), function(obj) {
+      return $(obj).attr("fileid");
+    });
+
+    $("#upload-file").on("click", function(e) {
+      const file = $('#pub-file-input')[0];
+
+      if (file.files[0]) {
+        uploadFile(file.files[0]).then(function(key) {
+          filesId.push(key)
+          let tmp = {};
+          tmp[`el:Report`] = {};
+          tmp[`el:Report`]["id"] = `${repId}`;
+          tmp[`el:Report`]["fileid/text"] = `${filesId}`;
+          let query = JSON.stringify(tmp);
+
+          $.ajax({
+            url: "/hquery/upsert",
+            async: false,
+            beforeSend: function(xhr) {
+              xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('token'));
+            },
+            data: query,
+            type: "POST",
+            success: (response) => {
+              console.log('upsert: ' + response);
+              location.reload();
+            }
+          });
+        });
+      }
+    })
   },
 }))
